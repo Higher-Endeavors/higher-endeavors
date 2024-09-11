0efb19dbe6f5221d877f1c44efcf9573776f51b0
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2024-09-11 14:44:24 -0500

    Backed out structural balance

--- src/app/tools/fitness/structural-balance/reference-lifts/[id]/edit/page.tsx
+++
@@ -1,5 +0,0 @@
-import EditRefLift from '../../../components/EditRefLift';
-
-export default function EditReferenceLiftPage({ params }: { params: { id: string } }) {
-  return <EditRefLift id={params.id} />;
-}
--- src/app/tools/fitness/structural-balance/reference-lifts/page.tsx
+++
@@ -1,5 +0,0 @@
-import ListRefLifts from '../components/ListRefLifts';
-
-export default function ReferenceLiftsPage() {
-  return <ListRefLifts />;
-}
