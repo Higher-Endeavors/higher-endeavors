3280e92660b6c6d746580c3e6ec9c58e993cc879
HighEndJC <jason.cockfield@me.com>
2024-09-16 18:02:13 -0500

    Created Guide Overview page structure. Awaiting final draft to finish code and generate pull request.

---
+++ src/app/(about)/guide/components/Guide.jsx
@@ -0,0 +1,9 @@
+const Guide = () => {
+    return (
+        <div className="container mx-auto px-12 py-8 lg:px-36 xl:px-72">
+            <h1 className="text-3xl font-bold mb-6">A Guide to Your Ideal Self: Your Journey Starts Here</h1>
+        </div>
+    );
+};
+
+export default Guide;
