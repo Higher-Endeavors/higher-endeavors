236de8c51691e99fb531bb4acd537b2d4ec71b48
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2024-10-01 16:14:15 -0500

    Added logo to signin page

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -4,6 +4,7 @@ import PostgresAdapter from "@auth/pg-adapter"
 import { pool } from "@/app/lib/dbAdapter"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   providers: [Cognito],
   adapter: PostgresAdapter(pool),
 })
