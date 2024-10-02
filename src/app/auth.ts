29d75c74830971a8304186728efc3dd95b28d885
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2024-10-02 08:53:58 -0500

    Declared session strategy

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -7,5 +7,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   providers: [Cognito],
   adapter: PostgresAdapter(pool),
+  session: {
+    strategy: "database"
+  },
   debug: true,
 })
