39aa0f12f085afa671b68ef2e5615106dedf4233
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2024-10-01 19:32:46 -0500

    Added session handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { auth } from '@/app/auth';
+import { SessionProvider } from "next-auth/react"
 
 import Header from './components/Header.jsx';
 import HeroBanner from './components/HeroBanner.jsx';
@@ -9,15 +10,23 @@ import Services from './components/Services.jsx';
 
 export default async function Home() {
   const session = await auth()
+  if (session?.user) {
+    session.user = {
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+    }
+  }
 
   return (
-    <div className="App">
-      <Header />
-      <HeroBanner />
-      <MissionStatement />
-      <Services />
-      <Footer />
-    </div>
+    <SessionProvider basePath={"/auth"} session={session}>
+      <div className="App">
+        <Header />
+        <HeroBanner />
+        <MissionStatement />
+        <Services />
+        <Footer />
+      </div>
+    </SessionProvider>
   );
 }
-  
