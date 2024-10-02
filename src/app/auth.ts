4bb9ed8f28772478fcdd28cbb0eca80206d97e6f
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2024-10-02 15:56:25 -0500

    Deleted cognito

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,11 +1,35 @@
 import NextAuth from "next-auth"
-import Cognito from "next-auth/providers/cognito"
+import Nodemailer from "next-auth/providers/nodemailer"
 import PostgresAdapter from "@auth/pg-adapter"
 import { pool } from "@/app/lib/dbAdapter"
- 
+
+const host = process.env.NEXT_PUBLIC_EMAIL_HOST
+const port = process.env.NEXT_PUBLIC_EMAIL_PORT
+const secure = (process.env.NEXT_PUBLIC_EMAIL_SECURE == "true") ? true : false
+const authMethod = process.env.NEXT_PUBLIC_EMAIL_AUTHMETHOD
+const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME
+const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD
+const fromEmail = process.env.NEXT_PUBLIC_FROM_EMAIL
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
-  providers: [Cognito],
+  providers: [
+    Nodemailer({
+      server: {
+        host: host,
+        port: port,
+        secure: secure,
+        authMethod: authMethod,
+    
+    auth: {
+      user: username,
+      pass: password,
+    },
+   },
+   from: fromEmail,
+  }),
+
+  ],
   adapter: PostgresAdapter(pool),
   session: {
     strategy: "database"
