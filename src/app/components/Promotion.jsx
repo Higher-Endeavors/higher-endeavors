1918947ca523c5b1f9f9e863bd814026e17a327b
HighEndJC <jason.cockfield@me.com>
2024-11-23 11:27:17 -0600

    Added Landing page Promotion Component

---
+++ src/app/components/Promotion.jsx
@@ -0,0 +1,15 @@
+const Promotion = () => {
+    return (
+      <section className="p-12">
+          <div className="bg-throat-chakra text-white pb-8 sm:text-md md:text-xl lg:text-2xl xl:text-4xl font-semibold rounded-3xl shadow-lg">
+            <p className="py-12 px-12 xl:px-24" >
+              Contact us to be put on our Beta Testing List.
+            </p>
+            <button className="hover:shadow-form rounded-md bg-purple-500 hover:bg-[#9400D3] py-3 px-8 text-base font-semibold text-white outline-none flex mx-auto">
+                Contact Us
+            </button>
+          </div>
+      </section>
+    );
+  }
+  export default Promotion
