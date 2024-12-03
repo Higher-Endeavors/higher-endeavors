106584b5c9aab2b40f5eb0ca0d5ce2de466ebc72
HighEndJC <jason.cockfield@me.com>
2024-12-03 10:46:45 -0600

    Deleted unnecessary console.log

--- src/app/(protected)/guide/[slug]/page.jsx
+++ src/app/(protected)/guide/[slug]/page.jsx
@@ -13,7 +13,6 @@ import Head from "next/head";
 export default async function Page({ params }) {
   const article = await getArticleBySlug(params.slug);
   const recentArticles = await getRecentArticles();
-  console.log('Recent articles in page component:', recentArticles);
 
   return (
     <SessionProvider>
