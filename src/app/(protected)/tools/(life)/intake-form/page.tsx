e4ebb7980e732128bd01692d9650fa01e6b8de7e
HighEndJC <jason.cockfield@me.com>
2024-12-20 09:45:09 -0600

    Added Header and Footer to Intake Form.

--- src/app/(protected)/tools/(life)/intake-form/page.tsx
+++ src/app/(protected)/tools/(life)/intake-form/page.tsx
@@ -1,11 +1,18 @@
 import IntakeForm from './IntakeForm';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
+import { SessionProvider } from 'next-auth/react';
 
 const IntakeFormPage = () => {
   return (
-    <div>
-      <h1 className="mx-auto px-12 py-8 lg:px-36 xl:px-72">User Intake Form</h1>
-      <IntakeForm />
-    </div>
+    <SessionProvider>
+      <div>
+        <Header />
+        <h1 className="mx-auto px-12 py-8 lg:px-36 xl:px-72">User Intake Form</h1>
+        <IntakeForm />
+        <Footer />
+      </div>
+    </SessionProvider>
   );
 };
 
