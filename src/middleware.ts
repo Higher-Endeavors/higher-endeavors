6fc1d74cdcd38f7c434b0248086e43ff2ffa28cf
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2025-04-02 09:38:50 -0500

    Fixed middleware for authjs

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,8 +13,14 @@ export function middleware(request: NextRequest) {
   if (publicGuideContent.includes(request.nextUrl.pathname)) {
     return NextResponse.next();
   }
+  let sessionTokenName = "";
+  if (process.env.LOCALHOST_DEV == "true") {
+    sessionTokenName = "authjs.session-token"
+  } else {
+    sessionTokenName = "__Secure-authjs.session-token"
+  }
 
-  if (request.cookies.has("__Secure-authjs.session-token")) {
+  if (request.cookies.has(`${sessionTokenName}`)) {
     return NextResponse.next();
   } else {
     return NextResponse.redirect(
