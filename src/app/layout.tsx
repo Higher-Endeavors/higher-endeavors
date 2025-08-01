819c9b398892d19a97277d36cea13db5ed6a7273
Mark Cockfield <100178558+MarkCockfield@users.noreply.github.com>
2025-08-01 12:20:02 -0500

    Added Google Analytics to global layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeModeScript } from "flowbite-react";
 import "./globals.css";
 import { UserSettingsProvider } from './context/UserSettingsContext';
 import { getUserSettings } from './lib/actions/userSettings';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 
 export const metadata: Metadata = {
@@ -16,6 +17,8 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const userSettings = await getUserSettings();
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+  
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -23,6 +26,7 @@ export default async function RootLayout({
         <link rel="stylesheet" href="https://use.typekit.net/pvb4enq.css" />
       </head>
       <body className="">
+        {gaId && <GoogleAnalytics gaId={gaId} />}
         <UserSettingsProvider userSettings={userSettings}>
           {children}
         </UserSettingsProvider>
