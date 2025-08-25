d5c23e1c9a5cd9bb08c335cf86d195d78031296c
HighEndJC <jason.cockfield@me.com>
2025-08-25 12:52:02 -0500

    Trying to fix EC2 build error.

--- src/app/(protected)/tools/(fitness)/resistance-training/components/ResistanceTraining.client.tsx
+++ src/app/(protected)/tools/(fitness)/resistance-training/components/ResistanceTraining.client.tsx
@@ -645,7 +645,6 @@ export default function ResistanceTrainingClient({
               setBaseWeekExercises([]);
               setActiveDay(1);
               setLockedWeeks(new Set());
-              setSaveResult(null);
               setIsTemplateProgram(false);
             }
           }}
