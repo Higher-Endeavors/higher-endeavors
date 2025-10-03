d9e66a974d34e69493033000808a35e0fa0f14d7
HighEndJC <jason.cockfield@me.com>
2025-10-03 14:50:35 -0500

    Fixed Server/ Client component issue that was causing an error when loading the Calendar.

--- src/app/(protected)/tools/calendar/page.tsx
+++ src/app/(protected)/tools/calendar/page.tsx
@@ -1,18 +1,18 @@
-'use client';
-
 import { SessionProvider } from 'next-auth/react';
 import { CalendarProvider } from '(protected)/tools/calendar/context/CalendarContext';
 import Calendar from '(protected)/tools/calendar/components/Calendar';
 import DemoBanner from '(protected)/tools/(components)/DemoBanner';
+import { getGarminDeviceAttribution } from 'lib/actions/userSettings';
 
-export default function CalendarPage() {
+export default async function CalendarPage() {
+  const garminAttribution = await getGarminDeviceAttribution();
   return (
     <SessionProvider>
       <CalendarProvider>
         <div className="w-full min-h-screen p-4">
           <div className="mx-auto max-w-7xl space-y-4">
             <DemoBanner />
-            <Calendar />
+            <Calendar garminAttribution={garminAttribution} />
           </div>
         </div>
       </CalendarProvider>
